7420c48b306645b4f1e893ef55d26b4d225373f3
Sudongk <aa88sd@naver.com>
2023-07-04 10:36:23 +0900

    #5 refactor : chatroom으로 테이블 이름 변경

--- src/main/resources/db/V1_init.sql
+++ src/main/resources/db/V1_init.sql
@@ -8,30 +8,26 @@ DROP TABLE IF EXISTS chat_room;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS user;
 
-create table user
-(
+create table user (
     id bigint auto_increment primary key,
     username varchar(20) not null unique,
     password varchar(255) not null,
     description varchar(255) default "",
-    create_at datetime(6) default now(),
-    modify_at datetime(6) default now()
+    create_at datetime default current_timestamp,
+    modify_at datetime default current_timestamp
 ) engine=InnoDB;
 
-create table chat_room
-(
+create table chatroom (
     id bigint auto_increment primary key,
     title varchar(255) not null,
     description varchar(255) default "",
-    create_at datetime(6) default now(),
-    modify_at datetime(6) default now(),
+    create_at datetime default current_timestamp,
+    modify_at datetime default current_timestamp,
     user_id bigint not null,
-    -- on delete cascade은 부모 테이블이 삭제되면 자식 테이블 값도 삭제되는 것이다.
-    foreign key (user_id) references user (id) on delete cascade
+    foreign key (user_id) references user (id)
 ) engine=InnoDB;
 
-create table categories
-(
+create table categories (
     id bigint AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(20) NOT NULL
 ) engine=InnoDB;
@@ -39,17 +35,18 @@ create table categories
 create table chatroom_categories (
     chatroom_id bigint,
     category_id bigint,
-    FOREIGN KEY (chat_room_id) REFERENCES chat_room(id) on delete cascade,
-    FOREIGN KEY (category_id) REFERENCES categories(id) on delete cascade,
-    PRIMARY KEY (chat_room_id, category_id)
+    FOREIGN KEY (chatroom_id) REFERENCES chatroom(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    PRIMARY KEY (chatroom_id, category_id)
 ) engine=InnoDB;
+
 
 create table chat_message (
     id bigint auto_increment primary key,
     message text,
-    create_at datetime(6) default now(),
-    chat_room_id bigint not null,
+    create_at datetime default current_timestamp,
+    chatroom_id bigint not null,
     user_id bigint not null,
-    foreign key (chat_room_id) references chat_room(id) on delete cascade,
-    foreign key (user_id) references user(id) on delete cascade
+    foreign key (chatroom_id) references chatroom(id),
+    foreign key (user_id) references user(id)
 ) engine=InnoDB;
