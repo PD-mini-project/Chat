6652659fe35d82a5e580e40bdf1d38ca24a70185
1996svibe <dndwlscjwo87@gmail.com>
2023-07-05 14:13:09 +0900

    init

--- src/main/java/com/chatting/room/user/domain/User.java
+++ src/main/java/com/chatting/room/user/domain/User.java
@@ -1,4 +1,44 @@
 package com.chatting.room.user.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", unique = true)
+    private String username;
+    @Column(name = "password", unique = true)
+    private String password;
+    @Column(name = "description")
+    private String description;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public User(String username, String password, String description, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.username = username;
+        this.password = password;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
--- src/main/java/com/chatting/room/user/repository/UserRepository.java
+++ src/main/java/com/chatting/room/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.chatting.room.user.repository;
 
-public class UserRepository {
+import com.chatting.room.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
 }
